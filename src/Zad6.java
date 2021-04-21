1c7759799f6cbc3d33ff3c6f4001b217b730253a
Mariusz Błaszczyk <mariusz.blaszczyk@tuta.io>
2021-04-21 21:45:09 +0200

    Zad6

--- src/Zad6.java
+++ src/Zad6.java
@@ -24,14 +24,16 @@ public class Zad6 {
         }
         return sum;
     }
-    
+
     static void printBiggerSum(int number1, int number2, int number3) {
         int sum1 = sumOfInterval(number1, number2);
         int sum2 = sumOfInterval(number2, number3);
-        if (sum1 < sum2) {
-            System.out.println("The sum of the first and second number is greater");
+        if (sum1 > sum2) {
+            System.out.println("The sum of the first and second number is greater.");
+        } else if (sum1 < sum2) {
+            System.out.println("The sum of the second and third number is greater.");
         } else {
-            System.out.println("The sum of the second and third number is greater");
+            System.out.println("The sums are equal.");
         }
     }
 
@@ -46,7 +48,8 @@ public class Zad6 {
             b = getNumberFromUser();
             System.out.println("Give the third number");
             c = getNumberFromUser();
-        } while (productThreeNumbers(a, b, c) <= 10 && a > b && b > c);
+        } while (!(productThreeNumbers(a,b,c) > 10 && a < b && b < c));
+
 
         printBiggerSum(a, b, c);
 
